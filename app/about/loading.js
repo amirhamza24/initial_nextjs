810ff8228deec75f04280e7df5c1fb8e1ae01896
amirhamza24 <ridoykhan7897890@gmail.com>
2025-07-10 17:59:58 +0600

    add loading

---
+++ app/about/loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Loading() {
+  console.log("hello loading");
+  return (
+    <div>
+      <div>Loading about segment...</div>
+    </div>
+  );
+}
