204fb23b16839602fb9427d8b646f6994864ee78
amirhamza24 <ridoykhan7897890@gmail.com>
2025-07-14 12:51:34 +0600

    error page add

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
+  throw new Error("There was an huge Error!");
 
   return (
-    <main className='mt-10'>
-      <div>
-        About us page
-      </div>
+    <main className="mt-10">
+      <div>About us page</div>
     </main>
-  )
+  );
 }
