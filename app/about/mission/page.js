4cafa7bbb9fc91999d13b6514aebf6446ad41332
amirhamza24 <ridoykhan7897890@gmail.com>
2025-08-31 22:42:26 +0600

    error not solve

--- app/about/mission/page.js
+++ app/about/mission/page.js
@@ -1,6 +1,8 @@
+// "use client";
+
 import Button from "@/app/components/Button";
-import Image from "next/image";
-import thumb from "@/public/images/next_img1.jpg";
+// import Image from "next/image";
+// import thumb from "@/public/images/first.jpg";
 
 export default function Mission() {
   return (
@@ -8,14 +10,16 @@ export default function Mission() {
       <div className="">Mission</div>
 
       <div>
-        Eos lorem dolor voluptua tempor ea sea erat sit sea et. Gubergren kasd
-        amet voluptua et sanctus sit no eirmod erat. Et gubergren lorem diam et
-        voluptua eos et takimata.
+        Lorem dolor voluptua consectetur tempor ea sea erat sit sea et.
+        Gubergren kasd amet voluptua et sanctus sit no eirmod erat. Et gubergren
+        lorem diam et voluptua eos et takimata.
       </div>
 
-      <Image src={thumb} alt="Thumb Image" />
+      {/* <div className="w-[400px] ">
+        <Image src={thumb} alt="Thumb Image" />
+      </div> */}
 
-      <Button />
+      {/* <Button /> */}
     </main>
   );
 }
