7cd50e3ca7dc5505857710413b780e12ff24f0a3
amirhamza24 <ridoykhan7897890@gmail.com>
2025-09-04 17:14:59 +0600

    dynamic metadata page wise

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,16 @@
 import getPost from "@/lib/getPost";
 import React from "react";
 
+export async function generateMetadata({ params }) {
+  const { id } = params;
+  const post = await getPost(id);
+
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+
 export default async function PostPage({ params }) {
   const { id } = params;
 
